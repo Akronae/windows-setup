7331ccff0590a3b1eae1e8a9f0a0fc3dce408ee1
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-08-31 12:26:10 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install git nvm nano googlechrome vscode notepad2
+choco install git nvm nano googlechrome firefox vscode notepad2
