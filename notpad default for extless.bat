7e517f448dd7be117fdefea3e6358be1ef9da450
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-09-01 18:44:10 +0200

    Update notpad default for extless.bat

--- notpad default for extless.bat
+++ notpad default for extless.bat
@@ -2,4 +2,17 @@
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
 assoc .=txtfile
+assoc .js=txtfile
+assoc .jsx=txtfile
+assoc .ts=txtfile
+assoc .tsx=txtfile
+assoc .html=txtfile
+assoc .xml=txtfile
+assoc .cs=txtfile
+assoc .css=txtfile
+assoc .less=txtfile
+assoc .sass=txtfile
+assoc .conf=txtfile
+assoc .local=txtfile
+assoc .development=txtfile
 pause
