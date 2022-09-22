bda3188d99885ccf7253d734f06c8fdcaec82715
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-09-22 15:03:47 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install git nvm nano googlechrome firefox vscode notepad2 cygwin qbittorrent
+choco install git nvm nano googlechrome firefox vscode notepad2 cygwin qbittorrent powertoys
 choco install coreutils --source=cygwin
 
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\tools\cygwin\bin;", "Machine")
