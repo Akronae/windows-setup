4c40b98bfe35de9b7ef3c0010a393ee653acca83
Alexandre Daubricourt <alexandre@daubricourt.com>
2023-06-27 16:52:07 +0200

    Update notpad default for extless.bat

--- notpad default for extless.bat
+++ notpad default for extless.bat
@@ -1,22 +1,22 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
-assoc .=txtfile
-assoc .svg=txtfile
-assoc .js=txtfile
-assoc .jsx=txtfile
-assoc .ts=txtfile
-assoc .tsx=txtfile
-assoc .html=txtfile
-assoc .xml=txtfile
-assoc .cs=txtfile
-assoc .css=txtfile
-assoc .less=txtfile
-assoc .sass=txtfile
-assoc .conf=txtfile
-assoc .local=txtfile
-assoc .development=txtfile
+assoc .="Text Document"
+assoc .svg="Text Document"
+assoc .js="Text Document"
+assoc .jsx="Text Document"
+assoc .ts="Text Document"
+assoc .tsx="Text Document"
+assoc .html="Text Document"
+assoc .xml="Text Document"
+assoc .cs="Text Document"
+assoc .css="Text Document"
+assoc .less="Text Document"
+assoc .sass="Text Document"
+assoc .conf="Text Document"
+assoc .local="Text Document"
+assoc .development="Text Document"
 
-ftype "txtfile"="c:\windows\system32\notepad.exe" "%1"
+ftype "Text Document"="c:\windows\system32\notepad.exe" "%1"
 
 pause
