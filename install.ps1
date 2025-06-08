f9eeef05ffe3821277975d7a4a877e7a965c59e3
Alexandre Daubricourt <alexandre@daubricourt.com>
2025-06-08 13:48:00 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -8,6 +8,12 @@ choco install coreutils zsh --source=cygwin
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\tools\cygwin\bin;", "Machine")
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\bin;", "Machine")
 
+# Set short date format to ISO 8601 (yyyy-MM-dd)
+Set-ItemProperty -Path "HKCU:\Control Panel\International" -Name "sShortDate" -Value "yyyy-MM-dd"
+# Set time format to 24-hour (HH:mm:ss)
+Set-ItemProperty -Path "HKCU:\Control Panel\International" -Name "sShortTime" -Value "HH:mm"
+Set-ItemProperty -Path "HKCU:\Control Panel\International" -Name "sTimeFormat" -Value "HH:mm:ss"
+
 remove-item alias:curl
 remove-item alias:nano
 remove-item alias:rm
