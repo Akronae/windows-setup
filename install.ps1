61967372483011633a4b2e5bcda1f8eee68905a1
Alexandre Daubricourt <alexandre@daubricourt.com>
2025-06-12 13:29:37 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,10 +3,9 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco feature enable -n allowGlobalConfirmation
 
 choco install -y googlechrome firefox brave vscode notepad2 cygwin qbittorrent powertoys slack discord sharex mongodb-compass parsec vlc bulk-crap-uninstaller 7zip 
-choco install coreutils zsh --source=cygwin
+choco install coreutils zsh git --source=cygwin
 
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\tools\cygwin\bin;", "Machine")
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\bin;", "Machine")
 
 # Set short date format to ISO 8601 (yyyy-MM-dd)
 Set-ItemProperty -Path "HKCU:\Control Panel\International" -Name "sShortDate" -Value "yyyy-MM-dd"
