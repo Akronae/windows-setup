a3ad4a5d70c714ce3e8d0f7fae89600f95d8270c
Alexandre Daubricourt <alexandre@daubricourt.com>
2025-06-26 14:19:43 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 choco feature enable -n allowGlobalConfirmation
 
-choco install -y googlechrome firefox brave vscode notepad2 cygwin qbittorrent powertoys slack discord sharex mongodb-compass parsec vlc bulk-crap-uninstaller 7zip 
+choco install -y googlechrome firefox brave vscode notepad2 cygwin qbittorrent powertoys slack discord sharex mongodb-compass parsec vlc bulk-crap-uninstaller 7zip ffmpeg
 choco install coreutils zsh git --source=cygwin
 
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\tools\cygwin\bin;", "Machine")
